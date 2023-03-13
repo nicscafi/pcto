dd1a814451d02e85207be3a1188c486aa325cad3
nicscafi <127745917+nicscafi@users.noreply.github.com>
2023-03-13 15:45:43 +0100

    Add files via upload

---
+++ esempio.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+int numero = 69;
+double numerolungo = 988989898';
+float numeroconlavirgola = 9.78;
+char carattere 'c';
+printf("il numero è %d\n , numero")
+( "ilnumero lungo è: %f, numero con la virgola è: %f\n", numerolungo, numeroconlavirgola)
+printf(" il carattere è %c/n , carattere") ;
+}
