16851e85832fd719e4ae4668835f8621423039db
nicscafi <127745917+nicscafi@users.noreply.github.com>
2023-03-14 10:51:49 +0100

    Add files via upload

--- iuliia.c
+++ iuliia.c
@@ -2,7 +2,14 @@
 int main()
 { 
     int eta;
+    char sesso;
+    
     printf("quanti anni hai?");
     scanf("%d", &eta);
    printf("il numero inserito è %d",eta );
+  printf("inserisci la tua eta");
+    scanf("%c", &sesso);
+   printf("io ho %d anni/n e sono %c/n", eta, sesso);
+
+      
     }
